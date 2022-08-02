9fb6b84124c5cfe3a66f357b13bf1cffe64c768b
LexTheBright <BrightComes@mail.ru>
2022-08-02 12:35:46 +0500

    Удалениеи JS папки из проекта

--- js/books.js
+++
@@ -1,9 +0,0 @@
-// const getBooked = () => {
-//   const getData = (value, category) => {
-//     fetch(`https://gutendex.com$/books`)
-//     .then((res) => res.json())
-//   }
-//   getData()
-// }
-
-// getBooked()
